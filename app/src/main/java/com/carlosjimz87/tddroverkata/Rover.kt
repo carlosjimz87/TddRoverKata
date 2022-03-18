2e05f5ca8b2de0d0e18162a1336e9b75fe99eec8
carlosjimz87 <carlosjimz87@gmail.com>
2022-03-19 00:51:23 +0100

    right and left

--- app/src/main/java/com/carlosjimz87/tddroverkata/Rover.kt
+++ app/src/main/java/com/carlosjimz87/tddroverkata/Rover.kt
@@ -3,19 +3,28 @@ package com.carlosjimz87.tddroverkata
 class Rover {
     private val x = 0
     private val y = 0
-    private val direction = "N"
-    val point = "$x:$y:$direction"
-
-    fun isPalindrome(candidate: String): Boolean {
-        return when (candidate) {
-            "Bob" -> true
-            "racecar" -> true
-            else -> false
-        }
-    }
+    private var d = 0
+    private val directions = listOf("N", "E", "S", "W")
 
     fun execute(commands: String): String {
-        return ""
+        if (commands.isNotEmpty()) {
+            commands.forEach { c ->
+                when (c) {
+                    'R' -> rotateRight()
+                    'L' -> rotateLeft()
+                }
+            }
+        }
+
+        return "$x:$y:${directions[d]}"
+    }
+
+    private fun rotateRight() {
+        d = (d + 1) % directions.size
+    }
+
+    private fun rotateLeft() {
+        d = if (d > 0) d - 1 else directions.size - 1
     }
 
     companion object {
--- app/src/test/java/com/carlosjimz87/tddroverkata/RoverTest.java
+++ app/src/test/java/com/carlosjimz87/tddroverkata/RoverTest.java
@@ -1,7 +1,6 @@
 package com.carlosjimz87.tddroverkata;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,15 +19,29 @@ public class RoverTest {
     }
 
     @Test
-    public void init_rover_position_is_0_0_N() {
-        assertThat(rover.getPoint(), is("0:0:N"));
+    public void init_position_is_0_0_N() {
+        assertThat(rover.execute("")).isEqualTo("0:0:N");
     }
 
     @Test
     @Parameters({
-            "R,0:0:E"
+            "R,0:0:E",
+            "RRR,0:0:W",
+            "RRRR,0:0:N",
+            "RRRRRRRR,0:0:N",
     })
     public void rotate_right(String commands, String position) {
-        assertThat(rover.execute(commands), is(position));
+        assertThat(rover.execute(commands)).isEqualTo(position);
+    }
+
+    @Test
+    @Parameters({
+            "L,0:0:W",
+            "LLL,0:0:E",
+            "LLLL,0:0:N",
+            "LLLLLLLL,0:0:N",
+    })
+    public void rotate_left(String commands, String position) {
+        assertThat(rover.execute(commands)).isEqualTo(position);
     }
 }
