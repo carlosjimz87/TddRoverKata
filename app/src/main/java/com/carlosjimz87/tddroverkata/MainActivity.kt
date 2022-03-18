56acdf1b1ad5853a19274d2863d027a4bc5f84f3
carlosjimz87 <carlosjimz87@gmail.com>
2022-03-18 22:43:12 +0100

    init

---
+++ app/src/main/java/com/carlosjimz87/tddroverkata/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.carlosjimz87.tddroverkata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
