4a93a0a3d44849a476379dc14105801e03b78f13
carlosjimz87 <carlosjimz87@gmail.com>
2022-03-19 14:45:49 +0100

    refactor Position

--- app/src/main/java/com/carlosjimz87/tddroverkata/Position.kt
+++ app/src/main/java/com/carlosjimz87/tddroverkata/Position.kt
@@ -1,10 +1,40 @@
 package com.carlosjimz87.tddroverkata
 
 class Position(
-    val x: Int = 0,
-    val y: Int = 0
+    private val x: Int = 0,
+    private val y: Int = 0
 ) {
+    companion object {
+        const val MAX_HEIGHT = 10
+        const val MAX_WIDTH = 10
+    }
+
     override fun toString(): String {
         return "$x:$y:"
+    }
+    
+    fun move(dir:Direction): Position {
+        return when (dir) {
+            Direction.EAST -> {
+                Position(inc(this.x ,MAX_WIDTH), this.y)
+            }
+            Direction.WEST -> {
+                Position(dec(this.x, MAX_WIDTH), this.y)
+            }
+            Direction.NORTH -> {
+                Position(this.x, dec(this.y, MAX_HEIGHT))
+            }
+            Direction.SOUTH -> {
+                Position(this.x, inc(this.y, MAX_HEIGHT))
+            }
+        }
+    }
+
+    private fun inc(value:Int ,max: Int): Int {
+        return (value + 1) % max
+    }
+
+    private fun dec(value:Int ,max: Int): Int {
+        return if (value > 0) value - 1 else max - 1
     }
 }
--- app/src/main/java/com/carlosjimz87/tddroverkata/Rover.kt
+++ app/src/main/java/com/carlosjimz87/tddroverkata/Rover.kt
@@ -16,7 +16,7 @@ class Rover {
                         direction = direction.left()
                     }
                     'M' -> {
-                        position = move()
+                        position = position.move(direction)
                     }
                 }
             }
@@ -24,29 +24,5 @@ class Rover {
         return "$position$direction"
     }
 
-    fun move(): Position {
-        when (direction) {
-            Direction.EAST -> {
-                val newPos = (position.x + 1) % MAX_WIDTH
-                return Position(newPos,position.y)
-            }
-            Direction.WEST -> {
-                val newPos = if (position.x > 0) position.x - 1 else MAX_WIDTH - 1
-                return Position(newPos,position.y)
-            }
-            Direction.NORTH -> {
-                val newPos = if (position.y > 0) position.y - 1 else MAX_HEIGHT - 1
-                return Position(position.x,newPos)
-            }
-            Direction.SOUTH -> {
-                val newPos = (position.y + 1) % MAX_HEIGHT
-                return Position(position.x,newPos)
-            }
-        }
-    }
 
-    companion object {
-        const val MAX_HEIGHT = 10
-        const val MAX_WIDTH = 10
-    }
 }
